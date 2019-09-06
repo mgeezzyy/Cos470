2b2e81b7b445a3fce9844d7ad34f9bef099ab6de
mgeezzyy <mi9uel9@gmail.com>
2019-09-06 11:37:10 -0400

    setting up files

---
+++ Assignment1/DollarWords/DollarWords/DollardWords.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DollarWords
+{
+    class DollardWordsProg
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
