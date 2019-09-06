d1943c0e3861f03a28eb85da4b4f6c9d5242b1bd
mgeezzyy <mi9uel9@gmail.com>
2019-09-06 11:39:35 -0400

    Calculates values for each word and prints dollar words

--- Assignment1/DollarWords/DollarWords/DollardWords.cs
+++ Assignment1/DollarWords/DollarWords/DollardWords.cs
@@ -1,12 +1,106 @@
 ﻿using System;
+using System.IO;
 
 namespace DollarWords
 {
-    class DollardWordsProg
+    class DollarWordsProg
     {
+        private int DWCount = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DollarWordsProg prog = new DollarWordsProg();
+            prog.ReadingTheFile();
+        }
+
+        public void ReadingTheFile()
+        {
+            using (var stream = File.OpenRead("C:\\Users\\windows_fausto\\Desktop\\cos470\\files\\words.txt"))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (reader.Peek() >= 0)
+                    {
+                        String buffer = reader.ReadLine();
+                        Boolean IsDollarWord = IsItADollarWord(buffer);
+                        if (IsDollarWord)
+                        {
+                            Console.WriteLine(buffer);
+
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public Boolean IsItADollarWord(String word)
+        {
+            int total = 0;
+            char[] lettersInWord = word.ToCharArray();
+            for (int i = 0; i < lettersInWord.Length; i++)
+                total += MapCharToValue(lettersInWord[i]);
+            if (total == 100)
+                return true;
+            return false;
+        }
+
+        public int MapCharToValue(char C)
+        {
+            if (C == 'A' || C == 'a')
+                return 1;
+            else if (C == 'B' || C == 'b')
+                return 2;
+            else if (C == 'C' || C == 'c')
+                return 3;
+            else if (C == 'D' || C == 'd')
+                return 4;
+            else if (C == 'E' || C == 'e')
+                return 5;
+            else if (C == 'F' || C == 'f')
+                return 6;
+            else if (C == 'G' || C == 'g')
+                return 7;
+            else if (C == 'H' || C == 'h')
+                return 8;
+            else if (C == 'I' || C == 'i')
+                return 9;
+            else if (C == 'J' || C == 'j')
+                return 10;
+            else if (C == 'K' || C == 'k')
+                return 11;
+            else if (C == 'L' || C == 'l')
+                return 12;
+            else if (C == 'M' || C == 'm')
+                return 13;
+            else if (C == 'N' || C == 'n')
+                return 14;
+            else if (C == 'O' || C == 'o')
+                return 15;
+            else if (C == 'P' || C == 'p')
+                return 16;
+            else if (C == 'Q' || C == 'q')
+                return 17;
+            else if (C == 'R' || C == 'r')
+                return 18;
+            else if (C == 'S' || C == 's')
+                return 19;
+            else if (C == 'T' || C == 't')
+                return 20;
+            else if (C == 'U' || C == 'u')
+                return 21;
+            else if (C == 'V' || C == 'v')
+                return 22;
+            else if (C == 'W' || C == 'w')
+                return 23;
+            else if (C == 'X' || C == 'x')
+                return 24;
+            else if (C == 'Y' || C == 'y')
+                return 25;
+            else if (C == 'Z' || C == 'z')
+                return 26;
+            else
+                return 0;
+
         }
     }
 }
