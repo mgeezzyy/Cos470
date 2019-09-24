ed4c6d97d8396f8456e16271ed040bc62d1f9562
mgeezzyy <mi9uel9@gmail.com>
2019-09-24 11:31:11 -0400

    needs testing

--- Assignment2/DollarAddresses/DollarAddresses/Program.cs
+++ Assignment2/DollarAddresses/DollarAddresses/Program.cs
@@ -12,28 +12,40 @@ namespace DollarAddresses
 {
     class DollarAddress
     {
+        private IConfigurationRoot Settings;
         static void Main(string[] args)
         {
-            DollarAddress app = new DollarAddress();
-            String url_config = "C:\\Users\\windows_fausto\\Desktop\\cos470\\ass0\\Cos470\\Assignment2\\DollarAddresses\\appsettings.json";
-            String address = app.CreateQuery(url_config);
-            String json_results = app.GetSerializedRecords(address);
-            var recs = JsonConvert.DeserializeObject<Address_records>(json_results);
-            foreach(var r in recs.features)
-            {
-                Console.WriteLine(r.attributes.STREETNAME);
-            }
-
+            RunApp();
         }
 
-        public String CreateQuery(String url_config)
+        public static void RunApp()
         {
-            var config = new ConfigurationBuilder()
+            String url_config = "C:\\Users\\windows_fausto\\Desktop\\cos470\\ass0\\Cos470\\Assignment2\\DollarAddresses\\appsettings.json";
+            DollarAddress app = new DollarAddress(url_config);
+            if (app.Settings["format"] != "pjson")
+                Environment.Exit(0);
+
+            String address = app.CreateQuery();
+            String json_results = app.GetSerializedRecords(address);
+            var recs = JsonConvert.DeserializeObject<Address_records>(json_results);
+            foreach (var r in recs.features)
+            {
+                if (app.IsItDollarAddress(r.attributes))
+                    Console.WriteLine(r.attributes.ADDRESS_NUMBER + " " + r.attributes.STREETNAME + " " + r.attributes.SUFFIX);
+            }
+        }
+
+        public DollarAddress(String url_config)
+        {
+            Settings = new ConfigurationBuilder()
                 .AddJsonFile(url_config, false, true)
                 .Build();
+        }
 
+        public String CreateQuery()
+        {
             var address = @"https://gis.maine.gov/arcgis/rest/services/Location/Maine_E911_Addresses_Roads_PSAP/MapServer/1/query?where=MUNICIPALITY%3D%27" 
-                + config["location"] + "%27&outFields=ADDRESS_NUMBER%2CSTREETNAME%2CSUFFIX%2CMUNICIPALITY&resultRecordCount=" + config["recordCount"] + "&f=" + config["format"];
+                + Settings["location"] + "%27&outFields=ADDRESS_NUMBER%2CSTREETNAME%2CSUFFIX%2CMUNICIPALITY&resultRecordCount=" + Settings["recordCount"] + "&f=" + Settings["format"];
 
             return address;
         }
@@ -43,6 +55,39 @@ namespace DollarAddresses
            var client = new WebClient();
            var content = client.DownloadString(address);
            return content;
+        }
+
+
+        public bool IsItDollarAddress(Address_records.Address address)
+        {
+            String street_name = address.STREETNAME;
+            String suffix = address.SUFFIX;
+            String address_ltrs = street_name + suffix;
+            int addr_number = address.ADDRESS_NUMBER;
+            if (addr_number == CalculateValue(address_ltrs))
+                return true;
+
+            return false;
+        }
+
+        public int CalculateValue(String name)
+        {
+            int total = 0;
+            name = name.ToLower();
+
+            foreach (char C in name)
+                total += MapCharToValue(C);
+
+            return total;
+        }
+
+        /* Maps A-Za-z characters to integers. */
+        public int MapCharToValue(char C)
+        {
+            if (C >= 'a' && C <= 'z')
+                return C - 'a' + 1;
+
+            return 0;
         }
 
         public class Address_records
