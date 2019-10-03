f5602ffa2e8a90fe6bc76c0dc052c91fe3adaa8e
mgeezzyy <mi9uel9@gmail.com>
2019-10-03 17:11:14 -0400

    added functions

---
+++ Assignment3/HaveWeMetLibrary/HaveWeMetLibrary/DateTimeHelpers.cs
@@ -0,0 +1,32 @@
+﻿using GeoCoordinatePortable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HaveWeMetLibrary
+{
+    class DateTimeHelpers
+    {
+        public static DateTime GetDateFromTimeStamp(string timestampMs)
+        {
+            var date = (new DateTime(1970, 1, 1)).AddMilliseconds(double.Parse(timestampMs));
+            var newDate = date.AddMilliseconds(-date.Millisecond);
+            return newDate;
+        }
+
+
+        public static bool LocationsCollide(double longitude1, double latitude1, double longitude2, double latitude2)
+        {
+            var coordinate1 = new GeoCoordinate(latitude1, longitude1);
+            var coordinate2 = new GeoCoordinate(latitude2, longitude2);
+            double distance = coordinate1.GetDistanceTo(coordinate2);
+            double thresholdOfCloseness = 2.0;
+
+            if (distance <= thresholdOfCloseness)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- Assignment3/HaveWeMetLibrary/HaveWeMetLibrary/LocationTracking.cs
+++ Assignment3/HaveWeMetLibrary/HaveWeMetLibrary/LocationTracking.cs
@@ -8,7 +8,15 @@ namespace HaveWeMetLibrary
     {
         public static void WhereWasI(LocationHistoryStruc locationHistory, DateTime date)
         {
-
+            foreach (var location in locationHistory.locations)
+            {
+                var date_buffer = DateTimeHelpers.GetDateFromTimeStamp(location.timestampMs);
+                if (DateTime.Compare(date, date_buffer) == 0)
+                    Console.WriteLine("On " + date + " your latitude was [" + location.latitudeE7
+                     + "] and your longitude was [" + location.longitudeE7 + "]");
+                else
+                    Console.WriteLine("We couldn't find you for the date: " + date);
+            }
         }
     }
 }
