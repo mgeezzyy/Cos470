3528382c9d124b73ce43cb914bcc77b99752ba26
mgeezzyy <mi9uel9@gmail.com>
2019-10-03 16:30:57 -0400

    added structure for json

---
+++ Assignment3/HaveWeMetLibrary/HaveWeMetLibrary/LocationHistory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HaveWeMetLibrary
+{
+        public class LocationHistoryStruc
+        {
+            public List<LocationData> locations { get; set; }
+
+            public class LocationData
+            {
+                public string timestampMs { get; set; }
+                public int latitudeE7 { get; set; }
+                public int longitudeE7 { get; set; }
+                public int accuracy { get; set; }
+                public List<Activity> activity { get; set; }
+            }
+
+            public class Activity
+            {
+                public string timestampMs { get; set; }
+                public List<ActivityInfo> activity { get; set; }
+            }
+
+            public class ActivityInfo
+            {
+                public string type { get; set; }
+                public int confidence { get; set; }
+            }
+        }
+
+}
---
+++ Assignment3/HaveWeMetLibrary/HaveWeMetLibrary/LocationTracking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaveWeMetLibrary
+{
+    class LocationTracking
+    {
+        public static void WhereWasI(LocationHistoryStruc locationHistory, DateTime date)
+        {
+
+        }
+    }
+}
